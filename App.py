789645d791ed1314da256d14e0c116cb1371db7a
leandro28coelho <lele28lima@gmail.com>
2025-04-28 17:35:02 -0300

    criando pagina html

--- App.py
+++ App.py
@@ -1,5 +1,48 @@
+from flask import Flask, render_template, request
+import sqlite3
+import hashlib
+
+app = Flask(__name__, static_url_path='/static/')
 
 
-print('ola mundo')
+def bate_papo():
+    conexao = sqlite3.connect("banco.db")
+    conexao.row_factory = sqlite3.Row
+    data = conexao.execute('SELECT * FROM Code ORDER BY ID DESC').fetchall()
+    return data
 
-print('ola novo')
+# Direciona para pagina Login
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        return render_template('index.html')
+
+
+    else:
+        return render_template('index.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+'''
+200 OK (OK)
+201 Created (Criado)
+204 No Content (Sem conteúdo)
+400 Bad Request (Requisição inválida)
+401 Unauthorized (Não autorizado)
+403 Forbidden (Proibido)
+404 Not Found (Não encontrado)
+405 Method Not Allowed (Método não permitido)
+409 Conflict (Conflito)
+500 Internal Server Error (Erro interno do servidor)
+502 Bad Gateway (Gateway ruim)
+503 Service Unavailable (Serviço indisponível)
+504 Gateway Timeout (Tempo limite do gateway)
+'''
