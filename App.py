2dd92679090e3f961318854165af7acf648117c1
leandro28coelho <lele28lima@gmail.com>
2025-05-02 13:24:41 -0300

    correcao e mudança no codigo

--- App.py
+++ App.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
 import sqlite3
 import hashlib
-VALOR=None
+VALOR='Anonymous'
 app = Flask(__name__, static_url_path='/static/')
 
 
 def limpeza():
-    # verifica a quantidade de registro no banco de dados / quando atingir 50 registros o banco é limpo
+    # verifica a quantidade de registro no banco de dados, quando atingir 50 registros o banco é limpo
     conexao = sqlite3.connect("banco.db")
     cursor = conexao.cursor()
     total = cursor.execute('SELECT COUNT(*) FROM Chat').fetchone()[0]
@@ -18,9 +18,8 @@ def limpeza():
     else:
         pass
 
-
+# mostra as conversas salva do banco de dados.
 def historico():
-    # mostra as conversas salva do banco de dados.
     conexao = sqlite3.connect("banco.db")
     conexao.row_factory = sqlite3.Row
     data = conexao.execute('SELECT * FROM Chat ORDER BY ID ASC').fetchall()
@@ -28,8 +27,8 @@ def historico():
     return data
 
 
+# cria o banco de dados e salva as conversas.
 def bate_papo(nome, texto):
-    # cria o banco de dados e salva as conversas.
     conexao = sqlite3.connect("banco.db")  # Banco de Dados
     curso = conexao.cursor()
 
@@ -51,7 +50,15 @@ def bate_papo(nome, texto):
         print('Erro ao salvar chat')
 
 
+@app.route('/mensagem', methods=['GET'])
+def mensagem():
+    print('rodou')
+    return render_template('mensagem.html', registros=historico())
 
+
+
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET', 'POST'])
 @app.route('/chat', methods=['GET', 'POST'])
 def chat():
     global VALOR
@@ -59,7 +66,7 @@ def chat():
     if request.method == 'POST':
         texto = request.form['texto']
         if texto == '':
-            return render_template('chat.html', aviso='Campo em Branco', registros=historico())
+            return render_template('chat.html', registros=historico())
 
         else:
             bate_papo(VALOR, texto)
@@ -68,28 +75,7 @@ def chat():
 
     # solicitação GET
     else:
-        if VALOR != None:
-            return render_template('chat.html', registros=historico())
-
-        else:
-            return render_template('index.html', aviso='Digite um nome para iniciar o chat')
-
-
-# Pagina principal
-@app.route('/', methods=['GET'])
-@app.route('/index', methods=['GET', 'POST'])
-def index():
-    global VALOR
-    if request.method == 'POST':
-        VALOR = request.form['nome']
         return render_template('chat.html', registros=historico())
-
-    else:
-        if VALOR != None:
-            return render_template('chat.html', registros=historico())
-
-        else:
-            return render_template('index.html', aviso='Digite um nome para iniciar o chat')
 
 
 def limpar():
